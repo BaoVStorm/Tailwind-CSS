87411b7ccc143100d7357e114b9b15c3e6aece79
VStorm <tranvubao2004@gmail.com>
2025-08-22 17:40:44 +0700

    add tailwind config

---
+++ 01_NetNinja/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: "blue"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
